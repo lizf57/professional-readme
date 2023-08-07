bc8041a586b67b01394a22f8c24727a2d565f696
mona lisa <lizfriedman5@gmail.com>
2023-08-07 18:45:46 -0500

    listing read me headers

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,12 +20,33 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none'){
+    return ` 
+    ## [License](#Table-of-Contents)
+    This project is covered under the following licence: ${renderLicenseLink(license)}`;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  *[Description](#description)
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[Contribution](#contribution)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ## [Description](#Table-of-Contents)
 `;
 }
 
