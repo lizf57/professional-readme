1daee1794b4ab3559c7d0391b2b5a860cc407857
mona lisa <lizfriedman5@gmail.com>
2023-08-07 19:42:43 -0500

    testing is working

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Professional README Generator Starter Code
+# Professional README Generator 
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+## Description
+The purpose of this project was to create a README template that is simple and professional. By designing a template, I can be more efficient and consistent by focusing on the important aspects needed to use and understand the application. 
+
+  ## Table-of-Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+
+  ## [Installation](#Table-of-Contents)
+  The link listed below is a direct link to the repository. 
+  
+  [README Generator Link](https://github.com/lizf57/professional-readme)
+
+  Once the link is clicked, you'll be brought to the repository on Github. Click the dropdown arrow on the green "code" button and clone the repo using your terminal. Once the repo is cloned, open it in VS code, or your choice of editor, and get started!
+
+
+  ## [Usage](#Table-of-Contents)
+  Once the application is installed, run the following code in the terminal: 'node index.js'. Answer the questions/prompts until a message says 'File created!'
+
+
+  ## [Contributions](#Table-of-Contents)
+  There are no other contributors for this project.
+
+  ## [Tests](#Table-of-Contents)
+  To test this applicatoin, open in VS code, or your prefered editor, and open the terminal. In the terminal, type 'node index.js' and answer the prompts.
+
+  ## [Questions](#Table-of-Contents)
+If you have any questions contact me here:
+
+  [GitHub](https://github.com/lizf57)
+  
+  [Email: lizfriedman5@gmail.com](mailto:lizfriedman5@gmail.com)
--- index.js
+++ index.js
@@ -3,13 +3,73 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    { 
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Write a short description of the project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions for the project.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What information does the user need to know about contribuing to the repo?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'How will the app be tested?',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license will the project have?',
+        name: 'license',
+        choices: ['Boost', 'BSD', 'Apache', 'MIT', 'none']
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let content = generatMarkdown(data);
+    fs.writeFile(fileName, content, function (error){
+        if (error){
+            return console.log(error)
+        }
+        console.log('file created!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data){
+        var fileName = 'README.md';
+        writeToFile(fileName, data)
+    });
+}
 
 // Function call to initialize app
 init();
